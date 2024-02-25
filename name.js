0539794c8fb02b559f4c719ec9fa30da28b33738
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-26 00:22:02 +0500

    Add files via upload

---
+++ name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['imaan', 'sonaina', 'maheen', 'ayesha'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ name.ts
@@ -0,0 +1,8 @@
+let names :string[] = ['imaan','sonaina','maheen','ayesha']
+
+
+for( let i=0; i<names.length; i++){
+
+    console.log(names [i])
+    
+}
